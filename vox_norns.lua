aee34819436d3e78102c34c4a59ef22c7e0d9f73
davidalovell <davidlovell@live.co.uk>
2021-10-05 16:36:02 +0100

    added chord fn

--- vox_norns.lua
+++ vox_norns.lua
@@ -6,8 +6,8 @@ sequins = include 'lib/sequins'
 Vox = include 'lib/vox'
 MusicUtil = require 'musicutil'
 
-function sc(scale) return MusicUtil.generate_scale_of_length(60, scale, 7) end
-userscale = sc('mixolydian')
+function scale(scale) return MusicUtil.generate_scale_of_length(60, scale, 7) end
+function chord(chord_type, inversion) return MusicUtil.generate_chord (1, chord_type, inversion) end
 usersynth = function(note, level) engine.hz(MusicUtil.note_num_to_freq(note)) end
 
 I = {1,3,5}
@@ -19,20 +19,20 @@ VI = {6,8,10}
 VII = {7,9,11}
 --
 
-
+-- 07879624907
 a = Vox:new{
   synth = usersynth,
-  scale = userscale,
+  scale = scale('mixolydian'),
   seq = {
     degree = sequins{1,3,5,7,9,11,13},
     sync = sequins{1/4,1/4,1/2},
     action = function()
       while true do
-        a:play(a.seq.dyn)
-        clock.sync(a.seq.dyn.sync())
+        a:play(a.seq.fn)
+        clock.sync(a.seq.fn.sync())
       end
     end,
-    dyn = {
+    fn = {
       degree = function() return a.seq.degree() end,
       sync = function() return a.seq.sync() end
     },
