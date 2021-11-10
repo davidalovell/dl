cf23c85bd3f00e5e44d1ffbfc9c378fe67427290
davidalovell <davidlovell@live.co.uk>
2021-11-10 22:16:49 +0000

    div and skip work

--- lib/seq2.lua
+++ lib/seq2.lua
@@ -1,4 +1,4 @@
-sequins = require('sequins_dl'); s = sequins
+sequins = require('sequins'); s = sequins
 
 Seq = {}
 
@@ -6,18 +6,21 @@ function Seq:new(args)
   local o = setmetatable( {}, {__index = Seq} )
   local args = args == nil and {} or args
 
-  o.s = args.s == nil and s{1,2,3,4} or args.s
-  o.div = args.div == nil and 1 or args.div -- sequins is only called on division
-  o.step = args.step == nil and 1 or args.step -- make it is so step is the same step as in sequins
-  o.skip = args.skip == nil and 1 or args.skip -- sequins is called, but result not returned
-  o.prob = args.prob == nil and 1 or args.prob -- sequins is called, but only returns if true
-  
-  o.offset = args.offset == nil and 0 or args.offset -- how many steps to wait before starting
-  o.action = args.action -- if there is a function then the sequins() is passed to the action, or just returns the sequins
+  o.s = args.s == nil and s{1,2,3,4,5,6,7} or s(args.s)
+  o.div = args.div == nil and 1 or args.div
+  o.step = args.step == nil and 1 or args.step
+  o.skip = args.skip == nil and 1 or args.skip
+  o.prob = args.prob == nil and 1 or args.prob
 
-  o.offset_count = - o.offset
-  o.div_count = 0
+  o.mod = args.mod == nil and 1 or args.mod
+  o.hold = args.hold == nil and false or args.hold
+
+  o.offset = args.offset == nil and 0 or args.offset
+  o.action = args.action
+
+  o.count = - o.offset
   o.val = 0
+  o.last = o.val
 
   return o
 end
@@ -32,65 +35,63 @@ function Seq:play(args)
 	  else
 	    updated_args[k] = v
 	  end
-		if updated_args[k] == nil then
+
+    -- for use with ALTERED sequins if you want to use the 'nested methods' out with nested sequins
+    if updated_args[k] == nil then
 		  return
 		end
-	  -- -- for use with unaltered sequins
+
+	  -- -- for use with UNALTERED sequins if you want to use the 'nested methods' out with nested sequins
 		-- if type(updated_args) == 'table' then
-		-- 	if updated_args[1] == nil then -- sequins returns an empty table so the first index will be negative in this instance
+		-- 	if updated_args[1] == nil then -- sequins returns an empty table so the first index will be nil in this instance
 		-- 		return
 		-- 	end
 		-- end
+
 	end
+
 	args = updated_args
 
-  local s, div, step, skip, prob
-  local next
+  local div, step, skip, prob
+  local mod, mod, hold
   
-  s = self.s
   div = self.div * (args.div == nil and 1 or args.div)
-  step = args.step == nil and self.step or args.step
+  step = args.step == nil and self.step or args.step; self.s:step(step)
   skip = args.skip == nil and self.skip or args.skip
   prob = args.prob == nil and self.prob or args.prob
 
-  self.s:step(step) -- untested
+  mod = args.mod == nil and self.mod or args.mod
+  hold = args.hold == nil and self.hold or args.hold
 
-  self.offset_count = self.offset_count + 1
+  self.count = self.count + 1
 
-  self.div_count = self.offset_count >= 1
-    and self.div_count % div + 1
-    or self.div_count
+  -- clean up nested ifs, have a single return
+  if self.count >=1 then
+    if self.count % div == mod % div then
+      self.val = self.s()
+    else
+      self.val = self.s[self.s.ix]
+      if not hold then
+        return
+      end
+    end
+    if self.count % skip == mod % skip and prob >= math.random() then
+      self.last = self.val
+      return self.val
+    else
+      if not hold then 
+        return
+      end
+      return self.last
+    end
+  end
+  -- need to somehow incorporate the action here
 
-  -- or just do some if statements
-  -- skip currently does what div should do
-  -- and div does nothing
-
-  -- self.val = self.offset_count >= 1 and self.div_count == 1 -- combine with next
-  --   and self.s()
-  --   or self.s[self.s.ix]
-    
-  next = (self.offset_count - 1) % skip == 0 and prob >= math.random()
-  self.val = next and self.s() or self.s[self.s.ix]
-	
-  return next and self.offset_count >= 1 and self.div_count == 1 and self.action ~= nil
-    and self.action(self.val)
-    or self.val
 end
 
 function Seq:reset()
-  self.offset_count = - self.offset
-  self.div_count = 0
+  self.count = - self.offset
+  self.val = 0
+  self.last = self.val
   self.s:reset()
-end
-
-function Seq:set_div()
-end
-
-function Seq:set_skip()
-end
-
-function Seq:set_prob()
-end
-
-function Seq:set_step()
 end
