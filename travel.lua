15f2938b86c27a10dd3a10890eab74561d7efd9c
davidalovell <davidlovell@live.co.uk>
2021-11-29 22:48:31 +0000

    travel

---
+++ travel.lua
@@ -0,0 +1,101 @@
+-- dl
+
+function reload()
+  norns.script.load('code/dl/travel.lua')
+end
+
+function r() norns.script.load(norns.script.state) end
+
+function key(n,z)
+  -- key actions: n = number, z = state
+  if n == 3 and z == 1 then
+    r()
+  end
+end
+
+sequins = include('lib/sequins_unnested'); s = sequins
+lattice = include('lib/lattice_1.2'); l = lattice:new()
+vox = include('lib/vox')
+seq = include('lib/seq')
+musicutil = require('musicutil')
+
+
+
+
+function sync(sync, fn)
+  return clock.run(function() clock.sync(sync); fn() end)
+end
+
+
+
+
+
+function clock.transport.start()
+  l:start()
+end
+
+function clock.transport.stop()
+  l:stop()
+  l:reset()
+  vox.call(voices, 'reset')
+end
+
+
+
+
+
+-- typical contstruct
+bass = vox:new{
+  synth = vox.midisynth,
+  device = midi.connect(1),
+  channel = 1,
+  on = true,
+  octave = 4,
+  scale = 'lydian',
+  length = 1/4
+}
+
+bass.user = {
+  cutoff = 0.5
+}
+
+bass.action = function(self, args)
+  args.user.cutoff = math.ceil(self.user.cutoff * args.user.cutoff() * 127)
+  args.device:cc(23, args.user.cutoff, args.channel)
+end
+
+bass.s = {
+  div = s{4,1,1,2},
+  cutoff = s{0.5,0.7,0.5,0.7,0.6}
+}
+
+bass.l = l:new_pattern{
+  division = 1/16,
+  action = function()
+    bass.seq:play{div = bass.s.div}
+  end
+}
+
+bass.seq = seq:new{
+  div = 2,
+  seq = {1,5,1,8},
+  action = function(val)
+    bass:play{degree = val, user = {cutoff = bass.s.cutoff}}
+  end
+}
+
+bass.device.event = function(data)
+  local msg = midi.to_msg(data)
+  if msg.type == 'cc' then
+    -- if msg.cc == 70 then
+    --   bass.seq.skip = math.floor(msg.val) + 1
+    -- end
+  end
+end
+
+
+
+
+
+
+voices = {bass}
